3327a7b72fa3d8da7e3c2135e3fff8a826238320
Seba <dese@student.chalmers.se>
2016-10-28 12:26:22 +0200

    git xd

---
+++ stonerkart/src/view/G.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stonerkart
+{
+    static class G
+    {
+        public static Map map = new Map(2, 2, false, false);
+
+        public static bool valid;
+
+        public static void load(Map m)
+        {
+            if (valid) throw new Exception();
+            map = m;
+
+            valid = true;
+        }
+
+        public static void unload()
+        {
+            valid = false;
+        }
+    }
+}
