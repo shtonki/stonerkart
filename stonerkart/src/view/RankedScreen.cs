72d85c579155f2e4726bb567653800dbe7d772a1
Seba <dese@student.chalmers.se>
2017-05-03 22:16:38 +0200

    modifierdoer now takes multiple modifiablestats

--- stonerkart/src/view/RankedScreen.cs
+++ stonerkart/src/view/RankedScreen.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace stonerkart
 {
-    public partial class RankedScreen : UserControl, Screen
+    partial class RankedScreen : StickyPanel, Screen
     {
         public RankedScreen()
         {
